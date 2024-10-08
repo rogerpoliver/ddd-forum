b990f2ebc414a8d132eb0aef169cbc9fa03b4c55
Roger Oliveira <rogerpolvr@gmail.com>
2024-10-07 23:59:18 -0300

    feat: add biome and fix all files

--- src/domain/use-cases/answer-question.spec.ts
+++ src/domain/use-cases/answer-question.spec.ts
@@ -1,15 +1,15 @@
-import { expect, test } from 'vitest';
+import { expect, test } from "vitest";
 
-import { AnswerQuestionsUseCase } from './answer-question';
+import { AnswerQuestionsUseCase } from "./answer-question";
 
-test('create an answer', () => {
-    const answerQuestion = new AnswerQuestionsUseCase()
+test("create an answer", () => {
+	const answerQuestion = new AnswerQuestionsUseCase();
 
-    const answer = answerQuestion.execute({
-        questionId: '1',
-        instructorId: '1',
-        content: 'New Answer'
-    })
+	const answer = answerQuestion.execute({
+		questionId: "1",
+		instructorId: "1",
+		content: "New Answer",
+	});
 
-    expect(answer.content).toEqual('New Answer')
-})
+	expect(answer.content).toEqual("New Answer");
+});
--- src/domain/use-cases/answer-question.ts
+++ src/domain/use-cases/answer-question.ts
@@ -1,14 +1,18 @@
-import { Answer } from '../entities/answer';
+import { Answer } from "../entities/answer";
 
 interface AnswerQuestionsUseCaseRequest {
-    instructorId: string
-    questionId: string
-    content: string
+	instructorId: string;
+	questionId: string;
+	content: string;
 }
 
 export class AnswerQuestionsUseCase {
-    execute({ instructorId, questionId, content }: AnswerQuestionsUseCaseRequest) {
-        const answer = new Answer(content)
-        return answer
-    }
+	execute({
+		instructorId,
+		questionId,
+		content,
+	}: AnswerQuestionsUseCaseRequest) {
+		const answer = new Answer(content);
+		return answer;
+	}
 }
