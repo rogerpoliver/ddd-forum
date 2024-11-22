b754bf6702dcdd388c626b8186c4acde86341a4b
Roger Oliveira <rogerpolvr@gmail.com>
2024-11-21 23:53:26 -0300

    test: add faker-js to the project

--- test/factories/make-question.ts
+++ test/factories/make-question.ts
@@ -1,18 +1,19 @@
+import { faker } from '@faker-js/faker';
+
 import { UniqueEntityID } from '../../src/core/entities/unique-entity-id.ts';
 import { Question, QuestionProps } from '../../src/domain/forum/enterprise/entities/question.ts';
-import { Slug } from '../../src/domain/forum/enterprise/entities/value-objects/slug.ts';
 
 export function makeQuestion(
-    override: Partial<QuestionProps>,
+    override: Partial<QuestionProps> = {},
+    id?: UniqueEntityID,
 ) {
     const question = Question.create({
         authorId: new UniqueEntityID(),
-        title: "Example question",
-        slug: Slug.create("example-question"),
-        content: "Example content",
         bestAnswerId: new UniqueEntityID(),
+        title: faker.lorem.sentence(),
+        content: faker.lorem.text(),
         ...override,
-    });
+    }, id);
 
     return question;
 }
