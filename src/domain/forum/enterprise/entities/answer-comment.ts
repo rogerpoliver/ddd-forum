22dba8b3c1413dd58cbf9502aae67bdbeebbd789
Roger Oliveira <rogerpolvr@gmail.com>
2024-12-30 17:47:02 -0300

    feat: create comments entities

---
+++ src/domain/forum/enterprise/entities/answer-comment.ts
@@ -0,0 +1,28 @@
+import { Optional } from "../../../../core/types/optional.ts";
+import { Comment, CommentProps } from "./comment.ts";
+
+import type { UniqueEntityID } from "../../../../core/entities/unique-entity-id.ts";
+
+export interface AnswerCommentProps extends CommentProps {
+    answerId: UniqueEntityID;
+}
+
+export class AnswerComment extends Comment<AnswerCommentProps> {
+    get answerId() {
+        return this.props.answerId;
+    }
+
+    static create(
+        props: Optional<AnswerCommentProps, "createdAt">,
+        id?: UniqueEntityID,
+    ) {
+        const answerComment = new AnswerComment(
+            {
+                ...props,
+                createdAt: props.createdAt ?? new Date(),
+            },
+            id,
+        );
+        return answerComment;
+    }
+}
---
+++ src/domain/forum/enterprise/entities/question-comment.ts
@@ -0,0 +1,28 @@
+import { Optional } from "../../../../core/types/optional.ts";
+import { Comment, CommentProps } from "./comment.ts";
+
+import type { UniqueEntityID } from "../../../../core/entities/unique-entity-id.ts";
+
+export interface QuestionCommentProps extends CommentProps {
+    questionId: UniqueEntityID;
+}
+
+export class QuestionComment extends Comment<QuestionCommentProps> {
+    get questionId() {
+        return this.props.questionId;
+    }
+
+    static create(
+        props: Optional<QuestionCommentProps, "createdAt">,
+        id?: UniqueEntityID,
+    ) {
+        const questionComment = new QuestionComment(
+            {
+                ...props,
+                createdAt: props.createdAt ?? new Date(),
+            },
+            id,
+        );
+        return questionComment;
+    }
+}
