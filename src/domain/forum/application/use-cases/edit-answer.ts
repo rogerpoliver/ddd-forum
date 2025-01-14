04e4d4fee1700a159a270e9761eebe5faf32a38e
Roger Oliveira <rogerpolvr@gmail.com>
2025-01-14 00:02:12 -0300

    refactor: apply funcional error handling to edit-answer use-case

--- src/domain/forum/application/use-cases/edit-answer.ts
+++ src/domain/forum/application/use-cases/edit-answer.ts
@@ -1,5 +1,8 @@
-import { Answer } from "../../enterprise/entities/answer.ts";
-import { AnswersRepository } from "../repositories/answers-repository.ts";
+import { Either, left, right } from '../../../../core/either.ts';
+import { Answer } from '../../enterprise/entities/answer.ts';
+import { AnswersRepository } from '../repositories/answers-repository.ts';
+import { NotAllowedError } from './errors/not-allowed-error.ts';
+import { ResourceNotFoundError } from './errors/resource-not-found-error.ts';
 
 interface EditAnswerUseCaseRequest {
     authorId: string;
@@ -7,29 +10,32 @@ interface EditAnswerUseCaseRequest {
     content: string;
 }
 
-interface EditAnswerCaseRequest {
-    answer: Answer;
-}
+type EditAnswerUseCaseResponse = Either<
+    ResourceNotFoundError | NotAllowedError,
+    {
+        answer: Answer;
+    }
+>;
 
 export class EditAnswerUseCase {
     constructor(private answersRepository: AnswersRepository) {}
 
     async execute(
         { authorId, answerId, content }: EditAnswerUseCaseRequest,
-    ): Promise<EditAnswerCaseRequest> {
+    ): Promise<EditAnswerUseCaseResponse> {
         const answer = await this.answersRepository.findById(answerId);
 
         if (!answer) {
-            throw new Error("Answer not found.");
+            return left(new ResourceNotFoundError());
         }
 
         if (authorId !== answer.authorId.toString()) {
-            throw new Deno.errors.PermissionDenied();
+            return left(new NotAllowedError());
         }
 
         answer.content = content;
 
         await this.answersRepository.save(answer);
-        return { answer };
+        return right({ answer });
     }
 }
