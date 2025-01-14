93d0fee1dde246a45dea553cca44551ee28bf7d3
Roger Oliveira <rogerpolvr@gmail.com>
2025-01-13 23:55:55 -0300

    refactor: apply funcional error handling to delete-answer-comment use-case

--- src/domain/forum/application/use-cases/delete-answer-comment.ts
+++ src/domain/forum/application/use-cases/delete-answer-comment.ts
@@ -1,29 +1,37 @@
+import { Either, left, right } from '../../../../core/either.ts';
 import { AnswersCommentsRepository } from '../repositories/answers-comments-repository.ts';
+import { NotAllowedError } from './errors/not-allowed-error.ts';
+import { ResourceNotFoundError } from './errors/resource-not-found-error.ts';
 
 interface DeleteAnswerCommentUseCaseRequest {
     authorId: string;
     answerCommentId: string;
 }
 
+type DeleteAnswerCommentUseCaseResponse = Either<
+    ResourceNotFoundError | NotAllowedError,
+    null
+>;
+
 export class DeleteAnswerCommentUseCase {
     constructor(private answersCommentsRepository: AnswersCommentsRepository) {}
 
     async execute(
         { authorId, answerCommentId }: DeleteAnswerCommentUseCaseRequest,
-    ) {
+    ): Promise<DeleteAnswerCommentUseCaseResponse> {
         const answersComment = await this.answersCommentsRepository.findById(
             answerCommentId,
         );
 
         if (!answersComment) {
-            throw new Error("Answer comment not found.");
+            return left(new ResourceNotFoundError());
         }
 
         if (authorId !== answersComment.authorId.toString()) {
-            throw new Deno.errors.PermissionDenied();
+            return left(new NotAllowedError());
         }
 
         await this.answersCommentsRepository.delete(answersComment);
-        return {};
+        return right(null);
     }
 }
